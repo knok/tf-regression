1c7ce8cfccc9de9fad4725deebc590479c2a87b2
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2020-02-19 16:26:19 +0900

    install modules and SudachiDict_core

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pip install -r requirements.txt
+pip install pip install https://object-storage.tyo2.conoha.io/v1/nc_2520839e1f9641b08211a5c85243124a/sudachi/SudachiDict_core-20200127.tar.gz
